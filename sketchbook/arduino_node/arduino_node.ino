1fbf1fdd6abee8fa82bd51bbfb437aa619f87551
Nick <nicholas.j.liu@gmail.com>
2020-12-29 08:10:17 +0000

    change encoder message to speed

--- sketchbook/arduino_node/arduino_node.ino
+++ sketchbook/arduino_node/arduino_node.ino
@@ -11,6 +11,16 @@ Servo servo;
 Servo motor;
 Encoder enc(3,2);
 
+unsigned long prevTime = 0;
+unsigned long currTime;
+
+long prevEncPos = -999;
+long encPos = -999;
+
+float ringBuffer[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+float runningAvg = 0;
+int ptr = 0;
+
 //msg: [-1.0, 1.0]
 void callback(const RC::pwmout& msg){
   servo.writeMicroseconds(percentToMicroSeconds(msg.steer));
@@ -25,8 +35,7 @@ int percentToMicroSeconds(double percent){
 ros::Subscriber<RC::pwmout> sub("pwmout", &callback);
 ros::Publisher pub("encoder", &encoderMessage);
 
-long encPos = -999;
-
+void setup() {
   nh.initNode();
   nh.advertise(pub);
   nh.subscribe(sub);
@@ -34,8 +43,20 @@ long encPos = -999;
 
 void loop() {
   encPos = enc.read();
-  encoderMessage.data = encPos;
-  pub.publish(&encoderMessage);
+  currTime = millis();
+
+  if (prevTime != 0) {
+    runningAvg -= ringBuffer[ptr] / 10.0;
+    runningAvg += (encPos - prevEncPos) / ((currTime - prevTime) / 1000.0) / 10.0;
+    ringBuffer[ptr] = (encPos - prevEncPos) / ((currTime - prevTime) / 1000.0);
+    ptr += 1;
+    ptr %= 10;
+
+    encoderMessage.data = runningAvg;
+    pub.publish(&encoderMessage);
+  }
+  prevTime = currTime;
+  prevEncPos = encPos;
   
   if (nh.connected()){
     if(!servo.attached()){servo.attach(10);}
--- upload.sh
+++ upload.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-~/arduino-1.8.7/arduino --upload --port /dev/ttyArduino ~/Arduino/sketchbook/arduino_node/arduino_node.ino
+
+~/arduino-1.8.9/arduino --upload --port /dev/ttyArduino ~/Arduino/sketchbook/arduino_node/arduino_node.ino
